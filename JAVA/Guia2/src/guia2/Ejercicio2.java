36e556588c2ac813b75983ef4c7734c79301a02f
“Camila <camigrael@gmail.com>
2023-03-10 10:41:29 -0300

    Version 1.0.6

---
+++ JAVA/Guia2/src/guia2/Ejercicio2.java
@@ -0,0 +1,14 @@
+
+package guia2;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main (String[] args){
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingresa tu nombre");
+        String nombre = leer.nextLine();
+        
+        System.out.println("Tu nombre es " + nombre);
+    }
+}
---
+++ JAVA/Guia2/src/guia2/Ejercicio5.java
@@ -0,0 +1,22 @@
+
+package guia2;
+import java.util.Scanner;
+//Para sacar la raiz cuadrada se puede importar:
+//import static java.lang.Math.sqrt;
+public class Ejercicio5 {
+    public static void main (String[] args){
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese un numero entero");
+        int num = leer.nextInt();
+        
+        int doble = num * 2;
+        int triple = num * 3;
+        //O solo colocar Math.sqrt();
+        double raiz = Math.sqrt(num);
+        
+        System.out.println("El doble de "+num+" es "+doble);
+        System.out.println("El triple de "+num+" es "+triple);
+        System.out.println("La raiz cuadrada de "+num+" es "+raiz);
+    }
+}
